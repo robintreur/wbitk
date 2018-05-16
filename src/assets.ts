b45f6e776b334ed053bf0bb4f2a99116ba7652be
robintreur <robintreur@gmail.com>
2018-05-16 14:28:27 +0200

    Added all assets to the project

---
+++ src/assets.ts
@@ -0,0 +1,47 @@
+export default class Assets {
+    constructor(scene : any) {
+        let assetsArray : Array<String> = [
+            "kitchenBar",
+            "kitchenBarEnd",
+            "kitchenBlender",
+            "kitchenCabinet",
+            "kitchenCabinetCornerInner",
+            "kitchenCabinetCornerRound",
+            "kitchenCabinetDrawer",
+            "kitchenCabinetUpper",
+            "kitchenCabinetUpperCorner",
+            "kitchenCabinetUpperDouble",
+            "kitchenCabinetUpperLow",
+            "kitchenCoffeeMachine",
+            "kitchenFridge",
+            "kitchenFridgeBuiltIn",
+            "kitchenFridgeSmall",
+            "kitchenMicrowave",
+            "kitchenSink",
+            "kitchenStove",
+            "kitchenStoveElectric"
+        ]
+
+        let assets = document.createElement("a-assets");
+        let path = "models/furniture/"
+        for(var key in assetsArray) {
+            let obj = this.createAsset(assetsArray[key], "obj", path)
+            assets.appendChild(obj)
+
+            let mtl = this.createAsset(assetsArray[key], "mtl", path)
+            assets.appendChild(mtl)
+        }
+        
+        scene.appendChild(assets)
+    }
+
+    createAsset(key: String, sort: String, path: String){
+        let el = document.createElement("a-asset-item")
+        let elName = key + "-" + sort;
+            
+        el.setAttribute("id", elName)
+        el.setAttribute("src", path + "" + key + "." + sort)
+        
+        return el;
+    }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,23 @@
-class Game {
+import 'aframe'
+import 'aframe-extras'
+
+import Assets from "./assets";
+
+export default class Game {
+  private scene : any;
+
   constructor() {
-    console.log("Loaded Game")
+    /**
+     * Create scene for Aframe
+     */
+    this.scene = document.createElement("a-scene")
+
+    /**
+     *  Load all 3d objects
+     */
+    new Assets(this.scene)
+
+    document.body.appendChild(this.scene)
   }
 }
 
