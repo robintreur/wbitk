7277a673426302032e45ca560eaa86726a23c849
robintreur <robintreur@gmail.com>
2018-05-16 15:37:01 +0200

    Added singleton

--- src/game.ts
+++ src/game.ts
@@ -4,21 +4,32 @@ import 'aframe-extras'
 import Assets from "./assets";
 
 export default class Game {
-  private scene : any;
+  private scene : any
+  private static instance: Game
 
-  constructor() {
+  private constructor() {
     /**
      * Create scene for Aframe
      */
     this.scene = document.createElement("a-scene")
 
     /**
-     *  Load all 3d objects
+     * Load all 3d objects
      */
     new Assets(this.scene)
 
     document.body.appendChild(this.scene)
   }
+
+  /**
+   * Singleton
+   */
+  public static getInstance() {
+    if (! Game.instance) {
+    Game.instance = new Game()
+    }
+    return Game.instance
+  }
 }
 
-window.addEventListener("load", () => new Game())
+window.addEventListener("load", () => Game.getInstance())
