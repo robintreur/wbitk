2c15664eeb8319724478514b3741476de320e1a3
robintreur <robintreur@gmail.com>
2018-06-01 14:29:14 +0200

    Added clickEvent & clickedAnimation

--- src/clickableModel.ts
+++ src/clickableModel.ts
@@ -20,13 +20,43 @@ export default class ClickableModel extends Model {
     }
 
     private cursorEnter(){
-        
+
         this.model.addEventListener("mouseenter", () => {
-            let scale = this.scale + 0.02
-            this.model.setAttribute("scale",  this.scale + " " + scale + " " + this.scale)
+            if(this.isKitchen){
+                let scale = this.scale + 0.02
+                this.model.setAttribute("scale",  this.scale + " " + scale + " " + this.scale)
+            }
+
+            document.body.onkeydown = (e) =>{
+                if(e.keyCode == 32){
+                    let position = 2;
+                    if(this.isFloor) position = 5
+
+                    let clicked = document.createElement("a-box")
+                    clicked.setAttribute("scale", 1 + " " + 1 + " " + 0.5)
+                    clicked.setAttribute("position",  + -position + " " + 0 + " " + position)
+                    clicked.setAttribute("rotation", -90 + " " + 0.1 + " " + 0)
+
+                    let AnimationLength = 30;
+                    for (let i = 0; i <= AnimationLength; i++) {
+                        setTimeout(() =>{
+                            clicked.setAttribute("scale", i*0.2 + " " + i*0.2 + " " + i*0.4);
+                            clicked.setAttribute("material", "transparent: true; opacity:" + (1 - ((1 / AnimationLength) * i)));
+
+                            if (i==AnimationLength) this.model.removeChild(clicked)
+                        }, i * 10);
+                    }
+
+                    this.model.appendChild(clicked)
+                }
+            }
         })
         this.model.addEventListener("mouseleave", () => {
-            this.model.setAttribute("scale",  this.scale + " " + this.scale + " " + this.scale)
+            if(this.isKitchen){
+                this.model.setAttribute("scale",  this.scale + " " + this.scale + " " + this.scale)
+            }
+
+            document.body.onkeydown = null;
         })
     }
 }
--- src/model.ts
+++ src/model.ts
@@ -1,15 +1,23 @@
 export default class Model {
     public model:any
+    public isFloor:boolean; 
+    public isWall:boolean;
+    public isKitchen:boolean;
 
     constructor(scene:any, name:String, posWidth:number, posHeight:number, posLength:number, rotation:number, scale:number) {
 
         this.model = document.createElement("a-obj-model")
         this.model.setAttribute("src", "#"+name+"-obj")
         this.model.setAttribute("mtl", "#"+name+"-mtl")
+        this.model.setAttribute("geometry")
         this.model.setAttribute("scale", scale + " " + scale + " " + scale)
         this.model.setAttribute("rotation", 0 + " " + rotation + " " + 0)
         this.model.setAttribute("position",  + posWidth + " " + posHeight + " " + posLength)
 
         scene.appendChild(this.model)
+
+        this.isFloor = this.model.getAttribute("src").includes("floor")
+        this.isWall =  this.model.getAttribute("src").includes("wall")
+        this.isKitchen =  this.model.getAttribute("src").includes("kitchen")
     }
 }
